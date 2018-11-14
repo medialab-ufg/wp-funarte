479e8ec403541cb857e49980d9a0f9efbbcbf16e
vnmedeiros <vnicius.nm.ba@gmail.com>
2018-11-14 17:54:55 -0200

    add taxomomy

--- wp-content/themes/funarte/inc/post_types/evento/evento.php
+++ wp-content/themes/funarte/inc/post_types/evento/evento.php
@@ -6,6 +6,7 @@ class Evento {
 
 	protected function init() {
 		add_action('init', array( &$this, "register_post_type" ));
+		add_action('init', array( &$this, "register_taxonomy" ));
 	}
 
 	public function register_post_type() {
@@ -44,6 +45,32 @@ class Evento {
 
 		register_post_type($POST_TYPE, $post_type_args);
 	}
+
+	function register_taxonomy() {
+        $labels = array(
+            'name' =>'Categorias de regionais',
+            'singular_name' => 'Categorias de regionais',
+            'search_items' => 'Buscar Categorias de regionais',
+            'all_items' => 'Todas as Categorias de regionais',
+            'parent_item' => 'Categorias de regionais Acima',
+            'parent_item_colon' => 'Categorias de regionais Acima:',
+            'edit_item' => 'Editar Categorias de regionais',
+            'update_item' => 'Atualizar Categorias de regionais',
+            'add_new_item' => 'Adicionar Nova Categorias de regionais',
+            'new_item_name' => 'Novo nome de Categorias de regionais',
+        );
+        register_taxonomy(
+            "regionais",
+            "evento",
+            array(
+                'hierarchical' => true,
+                'labels' => $labels,
+                'show_ui' => true,
+                'query_var' => true,
+                'rewrite' => false
+            )
+        );
+    }
 }
 
 Evento::get_instance();
