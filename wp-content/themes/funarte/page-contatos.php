54b758a0f0e5507945d6e004f28db05b4be6985c
Alvino Rodrigues <alvino.digo@gmail.com>
2018-11-19 17:42:09 -0200

    Pagina contatos  em construcao

---
+++ wp-content/themes/funarte/page-contatos.php
@@ -0,0 +1,119 @@
+<?php
+	get_header();
+?>
+
+<main role="main">
+	<a href="#content" id="content" name="content" class="sr-only">Início do conteúdo</a>
+	<div class="container">
+		<div class="box-breadcrumb">
+			<a class="box-breadcrumb__home" href="#"><i class="mdi mdi-home"></i></a>
+			<a href="#">Contatos</a>
+		</div>
+
+		<h2 class="title-h1">Contatos <span>Informações</span></h2>
+
+		<div class="list-boxes-info">
+			<section class="box-info">
+				<h3 class="title-h4">Programa Nacional de Apoio à Cultura | Pronac</h3>
+				<strong>Coordenador</strong>
+				<p>Ronaldo Daniel Gomes<br><a href="mailto:ronaldo.gomes@funarte.gov.br">ronaldo.gomes@funarte.gov.br</a></p>
+				<p>Centro Empresarial Cidade Nova – Teleporto<br>Av. Presidente Vargas nº 3.131 - 18º andar<br>Cidade Nova – Rio de Janeiro – RJ<br>CEP: 20.210-911</p>
+				<strong>Atendimento ao público</strong>
+				<p>(21) 2279-8111/8000/8114</p>
+			</section>
+
+			<section class="box-info">
+				<h3 class="title-h4">Centro de Artes Cênicas | Ceacen</h3>
+				<strong>Diretor</strong>
+				<p>Ginaldo Viana de Souza<br><a href="mailto:ceacen@funarte.gov.br">ceacen@funarte.gov.br</a></p>
+				<p>Centro Empresarial Cidade Nova – Teleporto<br>Av. Presidente Vargas nº 3.131 - 18º andar<br>Cidade Nova – Rio de Janeiro – RJ<br>CEP: 20.210-911</p>
+				<strong>Telefones</strong>
+				<p>(21) 2279-8010 / 2279-8012 / 2279-8013 / 2220-0032</p>
+
+				<div class="box-info__collapse">
+					<button type="button">Ocultar a estrutura de Centro de Artes Cênicas | Ceacen</button>
+					<div class="collapse__text">
+						<strong>Coteatro – Coordenação de Teatro</strong>
+						<em>Responsável pelos projetos da área de Teatro, pelo Teatro Glauce Rocha e pelo Teatro Dulcina<br>Coordenador de Teatro</em>
+						<p>Oscar José Ferreira da Costa Gonçalves</p>
+						<p>Centro Empresarial Cidade Nova – Teleporto<br>Av. Presidente Vargas nº 3.131 - 18º andar<br>Cidade Nova – Rio de Janeiro – RJ<br>CEP: 20.210-911</p>
+						<em>Telefones</em>
+						<p>(21) 2279-8018 / 2279-8095 / 2279-8100<br><a href="mailto:teatro@funarte.gov.br">teatro@funarte.gov.br</a></p>
+
+						<strong>Teatro Glauce Rocha</strong>
+						<em>Administrador</em>
+						<p>Carlos Benedito Souza Marques<br><a href="mailto:teatroglaucerocha@funarte.gov.br">teatroglaucerocha@funarte.gov.br</a></p>
+						<p>Avenida Rio Branco 179 – Centro<br>CEP: 20240-007 Rio de Janeiro – RJ</p>
+						<em>Telefones</em>
+						<p>(21) 2220-0259</p>
+
+						<strong>Teatro Dulcina</strong>
+						<em>Administrador</em>
+						<p>Salvador Fernando Pessanha<br><a href="mailto:teatrodulcina@funarte.gov.br">teatrodulcina@funarte.gov.br</a></p>
+						<p>Rua Alcindo Guanabara 17 – Cinelândia – Centro<br>Rio de Janeiro – RJ</p>
+						<em>Telefone</em>
+						<p>(21) 2240-4879</p>
+
+						<strong>Casa Funarte Paschoal Carlos Magno<br>Teatro Funarte Duse</strong>
+						<em>Administradora</em>
+						<p>Norma Dumar<br><a href="mailto:casapaschoalcarlosmagno.teatroduse@funarte.gov.br">casapaschoalcarlosmagno.teatroduse@funarte.gov.br</a></p>
+						<p>Rua Hermenegildo de Barros 161 – Santa Teresa<br>CEP: 20241-190 Rio de Janeiro – RJ</p>
+						<em>Telefone</em>
+						<p>(21) 2224-1163</p>
+
+						<strong>Aldeia de Arcozelo</strong>
+						<p><a href="mailto:ceacen@funarte.gov.br">ceacen@funarte.gov.br</a></p>
+						<em>Administrador</em>
+						<p>Aloísio José Leite de Medeiros</p>
+						<p>Avenida Embaixada Paschoal Carlos Magno 450 – Arco-zelo<br>CEP: 29650-000 Paty do Alferes – RJ</p>
+						<em>Telefone</em>
+						<p>(24) 2485-0198</p>
+
+						<strong>Codança – Coordenação de Dança</strong>
+						<em>Responsável pelos projetos da área de dança e pelo teatro<br>Cacilda Becker<br>Coordenador</em>
+						<p>Fabiano Carneiro da Silva</p>
+						<p>Centro Empresarial Cidade Nova – Teleporto<br>Av. Presidente Vargas nº 3.131 - 18º andar<br>Cidade Nova – Rio de Janeiro – RJ<br>CEP: 20.210-911</p>
+						<em>Telefone</em>
+						<p>(21) 2279-8014 / 2279-8097</p>
+						<em>Tel./fax:</em>
+						<p>(21) 2240-8907<br><a href="mailto:danca@funarte.gov.br">danca@funarte.gov.br</a></p>
+
+						<strong>Teatro Cacilda Becker</strong>
+						<p><a href="mailto:teatrocacildabecker@funarte.gov.br">teatrocacildabecker@funarte.gov.br</a><br><a href="mailto:danca@funarte.gov.br">danca@funarte.gov.br</a></p>
+						<em>Administrador</em>
+						<p>José Ricardo de Souza Malheiros<br><a href="mailto:ricardomalheiros@funarte.gov.br">ricardomalheiros@funarte.gov.br</a></p>
+						<p>Rua do Catete, 338 – Largo do Machado<br>CEP: 22220-001 – Rio de Janeiro – RJ</p>
+						<em>Telefone</em>
+						<p>(21) 2265-9933</p>
+
+						<strong>Coordenação de Circo</strong>
+						<em>Coordenador</em>
+						<p>Marcos Teixeira<br><a href="mailto:coordcirco@funarte.gov.br">coordcirco@funarte.gov.br</a></p>
+						<em>Telefone</em>
+						<p>(21) 2279-8034</p>
+						<p>Teatro Glauce Rocha<br>Av. Rio Branco, 179 – 7º Centro<br>CEP: 20240-007 Rio de Janeiro RJ</p>
+
+						<strong>Escola Nacional de Circo Luiz Olimecha</strong>
+						<em>Diretor</em>
+						<p>Carlos Eugênio Vianna Leite<br><a href="mailto:escolacirco@funarte.gov.br">escolacirco@funarte.gov.br</a><br>Rua Elpídio Boamorte, número 4, Praça da Bandeira – Rio de Janeiro – RJ</p>
+						<em>Telefones</em>
+						<p>2504-5561 e 2504-5496 (Direção); 2504-3720 (Coordena-ção); 2504-5320 (Secretaria Escolar)</p>
+						<strong>Fax</strong>
+						<p>(21) 2273-8567</p>
+
+						<strong>CTAC – Centro Técnico de Artes Cênicas</strong>
+						<em>Coordenador</em>
+						<p>Fernando Frascari</p>
+						<p>Rua do Lavradio,54 – Centro<br>CEP: 20230-070 Rio de Janeiro – RJ</p>
+						<strong>Tels./fax:</strong>
+						<p>(21) 2242-9808 / 2224-5382<br><a href="mailto:ctac@funarte.gov.br">ctac@funarte.gov.br</a></p>
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+</main>
+
+<?php
+	get_footer();
+?>
