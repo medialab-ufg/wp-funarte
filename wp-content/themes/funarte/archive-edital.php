5a104ae37c867e7fefd6593f4ac300a2da872872
Alvino Rodrigues <alvino.digo@gmail.com>
2018-11-22 11:08:49 -0200

    Estilizando a listagem de editais

--- wp-content/themes/funarte/archive-edital.php
+++ wp-content/themes/funarte/archive-edital.php
@@ -16,19 +16,43 @@ $Edital = \funarte\Edital::get_instance();
 $editais = $Edital->get_editais($status, $params);
 
 get_header();
-if (!empty($editais) && have_posts()):
 ?>
-	<ul><?php while (have_posts()): the_post(); ?>
-		<li>
-			<h6><?php echo $Edital->get_edital_status_name($post->ID); ?></h6>
-			<h4>
-				<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr(get_the_title()); ?>">
-					<?php the_title(); ?>
-				</a>
-			</h4>
-		</li>
-	<?php endwhile; ?></ul>
-<?php 
-endif;
+
+<main role="main">
+	<a href="#content" id="content" name="content" class="sr-only">Início do conteúdo</a>
+	<div class="container">
+		<?php include('inc/template_parts/breadcrumb.php'); ?>
+
+		<div class="box-title">
+			<h2 class="title-h1">Editais</h2>
+		</div>
+
+		<?php
+		if (!empty($editais) && have_posts()):
+		?>
+			<ul class="list-notices"><?php while (have_posts()): the_post(); ?>
+				<li>
+					<h6><?php echo $Edital->get_edital_status_name($post->ID); ?></h6>
+
+					<div class="link-area">
+						<a href="#"><?php the_category(); ?></a>
+					</div>
+					<h3 class="title-h5">
+						<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr(get_the_title()); ?>">
+							<?php the_title(); ?>
+						</a>
+					</h3>
+
+					<?php the_excerpt(); ?>
+				</li>
+			<?php endwhile; ?></ul>
+		<?php 
+		endif;
+		?>
+
+	</div>
+</main>
+
+<?php
 get_footer();
 ?>
---
+++ wp-content/themes/funarte/inc/template_parts/breadcrumb.php
@@ -0,0 +1,4 @@
+<div class="box-breadcrumb">
+	<a class="box-breadcrumb__home" href="#"><i class="mdi mdi-home"></i></a>
+	<a href="#">Editais</a>
+</div>
