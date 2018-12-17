8a1aae2c370c44dfd10165a79e07bf817915fed6
Alvino Rodrigues <alvino.digo@gmail.com>
2018-12-17 17:38:41 -0200

    botao de rolagem ao topo feito

--- wp-content/themes/funarte/archive-licitacao.php
+++ wp-content/themes/funarte/archive-licitacao.php
@@ -108,9 +108,9 @@ get_header();
 				</li>
 			<?php endwhile; ?>
 		</ul>
-		
+
 		<?php echo get_pagination(); ?>
-		
+
 	</div>
 	<?php endif;?>
 </main>
--- wp-content/themes/funarte/header.php
+++ wp-content/themes/funarte/header.php
@@ -11,6 +11,18 @@
 	</head>
 	
 	<body <?php echo $area ? "class='body-color-$area->slug'" : "" ; ?> >
+
+		<div id="barra-brasil" style="background:#7F7F7F; height: 20px; padding:0 0 0 10px;display:block;">
+			<ul id="menu-barra-temp" style="list-style:none;">
+				<li style="display:inline; float:left;padding-right:10px; margin-right:10px; border-right:1px solid #EDEDED">
+					<a href="http://brasil.gov.br" style="font-family:sans,sans-serif; text-decoration:none; color:white;">Portal do Governo Brasileiro</a>
+				</li>
+				<li>
+					<a style="font-family:sans,sans-serif; text-decoration:none; color:white;" href="http://epwg.governoeletronico.gov.br/barra/atualize.html">Atualize sua Barra de Governo</a>
+				</li>
+			</ul>
+		</div>
+
 		<!-- MENU DE ACESSIBILIDADE -->
 		<div class="accessibility-bar">
 			<nav class="container">
