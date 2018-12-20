beb088d77ace80f5aea264de7dc65edfe2a4b670
Alvino Rodrigues <alvino.digo@gmail.com>
2018-12-20 17:48:19 -0200

    Carrossel de destaques feito

--- wp-content/themes/funarte/inc/template_parts/carousel-highlights.php
+++ wp-content/themes/funarte/inc/template_parts/carousel-highlights.php
@@ -13,20 +13,6 @@ else:
 		<button type="button" class="control__next"><i class="mdi mdi-chevron-right"></i></button>
 		<button type="button" class="control__prev"><i class="mdi mdi-chevron-left"></i></button>
 	</div>
-	<div class="carousel-highlights__captions">
-		<?php
-			$id = 0;
-			foreach ($items as $item):
-		?>
-			<div class="carousel-highlights__caption carousel-highlights__caption-<?php echo $id; ?> <?php echo $id == 0 ? 'visible' : ''; ?>">
-				<strong><a href="<?php echo $item['url'];?>" title="<?php echo $item['title'];?>"><?php echo $item['title'];?></a></strong>
-				<span><a href="<?php echo $item['url'];?>" title="<?php echo $item['title'];?>"><?php echo $item['descricao'];?></a></span>
-			</div>
-		<?php
-			$id++;
-			endforeach;
-		?>
-				</div>
 	<ul class="carousel-highlights">
 		<?php foreach ($items as $item): ?>
 			<li>
@@ -36,5 +22,24 @@ else:
 			</li>
 		<?php endforeach; ?>
 	</ul>
+	<div class="carousel-highlights__captions">
+		<?php
+			$id = 0;
+			foreach ($items as $item):
+		?>
+			<div class="carousel-highlights__caption carousel-highlights__caption-<?php echo $id; ?> <?php echo $id == 0 ? 'visible' : ''; ?>">
+				<strong><a href="<?php echo $item['url'];?>" title="<?php echo $item['title'];?>"><?php echo $item['title'];?></a></strong>
+				<span><a href="<?php echo $item['url'];?>" title="<?php echo $item['title'];?>"><?php echo $item['descricao'];?></a></span>
+			</div>
+
+			<div class="hidden__caption hidden__caption-<?php echo $id; ?>">
+				<strong><a href="<?php echo $item['url'];?>" title="<?php echo $item['title'];?>"><?php echo $item['title'];?></a></strong>
+				<span><a href="<?php echo $item['url'];?>" title="<?php echo $item['title'];?>"><?php echo $item['descricao'];?></a></span>
+			</div>
+		<?php
+			$id++;
+			endforeach;
+		?>
+	</div>
 </section>
 <?php endif; ?>
