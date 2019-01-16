75741d0a6ee81eaa32f71d2550f2592aa482a4e0
Alvino Rodrigues <alvino.digo@gmail.com>
2019-01-16 15:57:10 -0200

    Lista de noticias em construcao

--- wp-content/themes/funarte/home.php
+++ wp-content/themes/funarte/home.php
@@ -30,29 +30,85 @@ query_posts(array_merge(array(
 			?>
 
 			<div class="box-title">
-				
-				<h2 class="title-h1">Notícias<span>Recentes</span></h2>
+				<h2 class="title-h1">Notícias<span>Mídias</span></h2>
 
 				<div class="box-forms">
-				<form class="form-area" action="#" method="get">
-					<fieldset>
-						<legend>Formulário de seleção de área</legend>
-						<?php
-						wp_dropdown_categories(array(
-							'show_option_none' => 'Filtrar por área',
-							'option_none_value' => '',
-							'hide_empty' => true,
-							'id' => 'select-categoria',
-							'class' => 'select_area',
-							'name' => 'area',
-							'value_field' => 'slug',
-							'selected' => (isset($categoria->slug)) ? $categoria->slug : null));
-						?>
-						
-					</fieldset>
-				</form>
+					<form class="form-area" action="#" method="get">
+						<fieldset>
+							<legend>Formulário de seleção de área</legend>
+
+							<select name="area" id="select-categoria" class="select_area">
+								<option value="" selected="selected">Filtrar por área</option>
+								<option class="level-0" value="funarte">Funarte</option>
+								<option class="level-0" value="artes-integradas">Artes Integradas</option>
+								<option class="level-0" value="artes-visuais">Artes Visuais</option>
+								<option class="level-0" value="circo">Circo</option>
+								<option class="level-0" value="danca">Dança</option>
+								<option class="level-0" value="literatura">Literatura</option>
+								<option class="level-0" value="musica">Música</option>
+								<option class="level-0" value="teatro">Teatro</option>
+								<option class="level-0" value="estudio-f">Estúdio F</option>
+							</select>
+						</fieldset>
+					</form>
+				</div>
+			</div>
+
+			<div class="row justify-content-between mb-90">
+				<div class="col-md-4">
+					<div class="box-bidding box-bidding--type-b">
+						<ul class="list-bidding--type-b">
+							<li class="color-funarte">
+								<div class="link-area">
+									<a href="#">Facebook</a>
+								</div>
+								<strong>Título lorem ipsum sit dolor amet, con-sectetur adispicing elit, sed do</strong>
+								<a class="link-more" href="#">Ler mais</a>
+							</li>
+							<li class="color-funarte">
+								<div class="link-area">
+									<a href="#">Twitter</a>
+								</div>
+								<strong>Título lorem ipsum sit dolor amet, con-sectetur adispicing elit, sed do</strong>
+								<a class="link-more" href="#">Ler mais</a>
+							</li>
+							<li class="color-funarte">
+								<div class="link-area">
+									<a href="#">Facebook</a>
+								</div>
+								<strong>Título lorem ipsum sit dolor amet, con-sectetur adispicing elit, sed do</strong>
+								<a class="link-more" href="#">Ler mais</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 
+				<div class="col-md-7">
+					aasdf
+				</div>
+			</div>
+
+			<div class="box-title">
+				<h2 class="title-h1 mb-30">Notícias<span>Recentes</span></h2>
+
+				<div class="box-forms">
+					<form class="form-area" action="#" method="get">
+						<fieldset>
+							<legend>Formulário de seleção de área</legend>
+							<?php
+							wp_dropdown_categories(array(
+								'show_option_none' => 'Filtrar por área',
+								'option_none_value' => '',
+								'hide_empty' => true,
+								'id' => 'select-categoria',
+								'class' => 'select_area',
+								'name' => 'area',
+								'value_field' => 'slug',
+								'selected' => (isset($categoria->slug)) ? $categoria->slug : null));
+							?>
+						</fieldset>
+					</form>
+				</div>
 			</div>
 		</div>
 
