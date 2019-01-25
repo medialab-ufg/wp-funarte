dee7b4a8a9b3f5fb15bf6343ef478dfa862771e2
vnmedeiros <vnicius.nm.ba@gmail.com>
2019-01-25 16:03:58 -0200

    box-carousel-image comment

--- wp-content/themes/funarte/single.php
+++ wp-content/themes/funarte/single.php
@@ -36,6 +36,32 @@ if(have_posts()) : the_post();
 				</div>
 			</div>
 
+			<!-- <section class="box-carousel-image">
+				<div class="carousel-image__wrapper">
+					<div class="carousel-image__control">
+						<button type="button" class="control__next"><i class="mdi mdi-chevron-right"></i></button>
+						<button type="button" class="control__prev"><i class="mdi mdi-chevron-left"></i></button>
+					</div>
+					<ul class="carousel-image">
+						<li>
+							<a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/img/fke/news_003.jpg'; ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/img/fke/news_003.jpg'; ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/img/fke/news_003.jpg'; ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/img/fke/news_003.jpg'; ?>" alt=""></a>
+						</li>
+						<li>
+							<a href="#"><img src="<?php echo get_template_directory_uri() . '/assets/img/fke/news_003.jpg'; ?>" alt=""></a>
+						</li>
+					</ul>
+				</div>
+			</section> -->
+
 			<?php
 				$tax = \Tainacan\Repositories\Taxonomies::get_instance()->fetch_one(['slug'=>'assunto']);
 				$slug_taxonomia = $tax->get_db_identifier();
