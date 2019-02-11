14f8178da64893e7fa669d39bf86ab8dd4d5d14d
luisfeliperd <luisfelipeprf@gmail.com>
2019-02-11 10:09:43 -0200

    Update estudio_f.php

--- migration_scripts/estudio_f.php
+++ migration_scripts/estudio_f.php
@@ -9,6 +9,9 @@ $posts = new WP_Query([
     'posts_per_page' => -1
 ]);
 
+#Gerar documentos anexados no TAINACAN - Seta o primeiro como document_id e os outros dois muda o post_parent e coloca o ID do item#
+
+
 
 function scrapeImage($text) {
     $pattern = '/src=[\'"]?([^\'" >]+)[\'" >]/';
@@ -19,45 +22,58 @@ function scrapeImage($text) {
 
 }
 
-#var_dump( attachment_url_to_postid('http://localhost/funarte/wp-content/uploads/2016/12/EF-324-Novos-Baianos-bloco-3.mp3') ); die;
-
 while ($posts->have_posts()) {
     
     $posts->the_post();
     
-    #echo get_permalink($post); #Link do Post
-    #echo 'Post ID - ', $post->ID, ' '; # ID do post
+##Variáveis dos Posts##
+    
+	#post->ID; #ID do post
+    echo $title = $post->post_title, "\n"; #Título do Post
+    #$post_link = get_permalink($post); #Link do Post
+    #$post_content = $post->post_content; #Conteúdo do Post - !Verificar como remover parte indesejada do conteúdo.!
     
 
-    #Verificando se tem documentos anexados ao post.
+##Documentos Anexados aos Posts (Audios)##
+
+    #Verificando a existencia de documentos anexados ao post.
 
     $att = $media = get_attached_media('audio');
     
+	$count_att = 0;
+	$count_link = 0;
     if ($att) {
-        // seta o primeiro como document_id e os outros dois muda o post_parent e coloca o ID do item
-		foreach ($att as $atch) {
-			#echo 'Att ID - ', $atch->ID, ' ';
-			
-			}
- 
-    } else {
 		
-
+		foreach ($att as $atch) {
+			$count_att++;
+			echo $count_att, ' - ';
+			echo $atch->ID; #IDs dos documentos anexados ao post.
+			echo "\n";
+		}
+ 
+    } else{
+		
+	#Verificando os posts com documentos anexados por link no conteúdo.
 		
 		$content = $post->post_content;
-		echo preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $content, $matches);
+		preg_match_all('#\bhttps?://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))#', $content, $matches);
 		$links = $matches[0];
 		
 		foreach ($links as $link){
+			
+			
 			if (strpos($link, '.mp3') !== false){
-				echo attachment_url_to_postid($link), "\n";
+				$count_link++;
+				
+				echo $count_link, ' - ';
+				echo attachment_url_to_postid($link); #IDs dos documentos anexados ao post via link.
+				echo "\n";
 			}
 		}
 			
-	}die;
+	}
     
     
-    // seta o primeiro como document_id e os outros dois muda o post_parent e coloca o ID do item
     
     echo "\n";
     
