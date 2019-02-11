734be221119ac52aff7cf08c6b4715333be8ead2
vnmedeiros <vnicius.nm.ba@gmail.com>
2019-02-11 15:51:53 -0200

    fix get the page template on save

--- wp-content/themes/funarte/inc/meta-boxes/metabox-arquivos-diversos.php
+++ wp-content/themes/funarte/inc/meta-boxes/metabox-arquivos-diversos.php
@@ -23,6 +23,7 @@ class MetaboxArquivosDiversos {
 	}
 
 	public function save_custom_box($post_id) {
+		$page_template = get_page_template_slug( $post_ID );
 		if (!in_array($page_template, $this->allowed_templates)) {
 			return $post_id;
 		}
