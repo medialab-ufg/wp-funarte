c0c981f5c7153639c6111b5ba2ead1f6a67acd92
Alvino Rodrigues <alvino.digo@gmail.com>
2019-02-12 18:27:50 -0200

    Lista de audios em colecoes em construcao

--- wp-content/themes/funarte/assets/js/base.js
+++ wp-content/themes/funarte/assets/js/base.js
@@ -51,6 +51,9 @@ $(document).ready(function() {
 	base.calendario.ativar();
 	base.calendario.ativarCompacto();
 	base.calendario.atualizaCarrosselCompacto();
+
+	// Lista de coleções de áudios
+	base.audio.ativarListaColecao();
 });
 
 var base = {
@@ -63,6 +66,23 @@ var base = {
 					elements: ['controls', 'time', 'volume', 'progress']
 				});
 			}
+		},
+
+		ativarListaColecao: function() {
+			//setTimeout(function(){
+				console.log('ativou');
+				var $audioPlayer = $('.audios-list__play');
+
+				//if ($audioPlayer.length > 0) {
+					console.log('length');
+					$audioPlayer.on('click',function() {
+						console.log('click');
+						$(this).siblings('.audios-list__audio').musicPlayer({
+							elements: ['controls', 'time', 'volume', 'progress']
+						});
+					});
+				//}
+			//}, 3000);
 		}
 	},
 
--- wp-content/themes/funarte/tainacan/audio-players-view-mode.php
+++ wp-content/themes/funarte/tainacan/audio-players-view-mode.php
@@ -1,20 +1,28 @@
 <?php if ( have_posts() ) : ?>
-	
-	
+	<ul class="audios-list">
 	<?php while ( have_posts() ) : the_post(); ?>
-		
-		
-		<?php the_title(); ?>
-		
-		<?php echo funarte_get_document_url(); ?>
-		
-		<br/>
-		<br/>
-		
-		
+		<li>
+			<div class="audios-list__box">
+				<button type="button" class="audios-list__play"><i class="mdi mdi-play-circle-outline"></i></button>
+				<ul class="audios-list__audio">
+					<li><a href="<?php echo funarte_get_document_url(); ?>"></a></li>
+				</ul>
+
+				<div class="audios-list__title">
+					<strong><?php the_title(); ?></strong>
+				</div>
+
+				<div class="audios-list__columns">
+					<div class="audios-list__image" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></div>
+
+					<div class="audios-list__text">
+						<?php the_content(); ?>
+					</div>
+				</div>
+			</div>
+		</li>
 	<?php endwhile; ?>
-	
-	
+	</ul>
 <?php else: ?>
 	
 	Nada
