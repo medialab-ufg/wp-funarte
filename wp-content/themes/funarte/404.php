790b275e7ef8a4721e3377fb20dcffc36a6e4901
Alvino Rodrigues <alvino.digo@gmail.com>
2019-04-01 18:26:15 -0300

    Pagina 404 criada

---
+++ wp-content/themes/funarte/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<div class="container">
+			<div class="box-error-404">
+				<strong>Página não encontrada</strong>
+				<h2 class="box-error-404__title">Erro <span>404</span></h2>
+			</div>
+		</div>
+	</main>
+
+<?php get_footer(); ?>
