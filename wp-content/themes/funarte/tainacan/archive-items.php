d89c21e4e1e8080eb9b4f90471bc367498407363
Leo Germani <leogermani@hacklab.com.br>
2019-04-29 17:09:51 -0300

    adiciona descrição da coleção

--- wp-content/themes/funarte/tainacan/archive-items.php
+++ wp-content/themes/funarte/tainacan/archive-items.php
@@ -8,7 +8,12 @@
 	<div class="container mb-100">
 		<div class="box-title">
 			<h2 class="title-h1">Acervo <span><?php echo tainacan_get_the_collection_name(); ?></span></h2>
+			
 		</div>
+		
+		<p>
+			<?php echo apply_filters('the_content', tainacan_get_the_collection_description()); ?>
+		</p>
 
 		<?php tainacan_the_faceted_search(); ?>
 	</div>
