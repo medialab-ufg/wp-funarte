56a8f4ef6a71d19fb59c90b1c925893a29aa43b8
Alvino Rodrigues <alvino.digo@gmail.com>
2019-05-21 11:55:22 -0300

    Corrigindo as noticias das internas das areas

--- wp-content/themes/funarte/inc/template_parts/box-news.php
+++ wp-content/themes/funarte/inc/template_parts/box-news.php
@@ -43,7 +43,7 @@ else:
 							<a href='" . $item['url'] . "'><span class='box-news__image' style='background-image: url($url_img);'></span></a>
 							<h3 class='news-title'><a href='" . $item['url'] . "'>" . $item['title'] . "</a></h3>
 							<p class='news-subtitle'>" . $subtitle . "</p>
-							<span>" . $content . "</span>
+							<p>" . $content . "</p>
 							<a href='" . $item['url'] . "' class='link-more'>Ler mais</a>
 						</li>";
 	endforeach;
