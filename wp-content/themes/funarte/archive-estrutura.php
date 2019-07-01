de842b842dafffc7699a3124b77e32c2b4e5106a
Alvino Rodrigues <alvino.digo@gmail.com>
2019-07-01 17:31:15 -0300

    Inserido o terceiro nivel na pagina de estrutura.

--- wp-content/themes/funarte/archive-estrutura.php
+++ wp-content/themes/funarte/archive-estrutura.php
@@ -7,6 +7,14 @@
 		'order' => 'ASC',
 		'post_parent' => 0
 	));
+	function FUNARTE_get_childrens_estrutura($ID) {
+		$arg = array(
+			'post_parent' => $ID,
+			'post_type'   => 'estrutura',
+			'numberposts' => -1,
+			'post_status' => 'any');
+		return get_children($arg);
+	}
 ?>
 
 <main role="main">
@@ -26,23 +34,33 @@
 					<h3 class="title-h4"><?php the_title(); ?></h3>
 					<?php 
 						the_content();
-						$arg = array(
-							'post_parent' => get_the_ID(),
-							'post_type'   => 'estrutura',
-							'numberposts' => -1,
-							'post_status' => 'any');
-						$filhos = get_children($arg);
+						$filhos = FUNARTE_get_childrens_estrutura(get_the_ID());
 						if(!empty($filhos)): ?>
 							<div class="box-info__collapse">
 								<button class="collapse__button" type="button">Exibir a estrutura de <?php the_title() ?></button>
 								<div class="collapse__text">
-									<?php foreach($filhos as $filho): ?>
+									<?php foreach($filhos as $filho_ID => $filho): ?>
 										<div class="text__block">
 											<strong><?php echo $filho->post_title; ?></strong>
 											<?php 
 												$content = $filho->post_content; 
 												$content = apply_filters('the_content', $content);
 												echo $content;
+
+												$netos = FUNARTE_get_childrens_estrutura($filho_ID);
+												
+												foreach($netos as $neto):
+											?>
+													<button class="collapse__button" type="button">Exibir a estrutura de <?php echo $filho->post_title; ?></button>
+													<div class="collapse__text">
+														<?php
+															echo '<strong>' . $neto->post_title . '</strong>';
+															$content = $neto->post_content; 
+															$content = apply_filters('the_content', $content);
+															echo $content;
+														?>
+													</div>
+											<?php endforeach;
 											?>
 										</div>
 									<?php endforeach; ?>
